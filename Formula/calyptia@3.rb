ce80078ee8c879d673566e62bbbf43a479dccad6
Pat <pat@chronosphere.io>
2024-07-05 09:44:57 -0400

    Fix incorrect URL in release 3.0.2 (#5)
    * Fix incorrect URL in release 3.0.2
    
    Signed-off-by: Patrick Stephens <pat@chronosphere.io>
    
    * Fix versions
    
    Signed-off-by: Patrick Stephens <pat@chronosphere.io>
    
    ---------
    
    Signed-off-by: Patrick Stephens <pat@chronosphere.io>

--- Formula/calyptia@3.rb
+++ Formula/calyptia@3.rb
@@ -9,7 +9,7 @@ class CalyptiaAT3 < Formula
   license "Apache-2.0"
 
   on_macos do
-    url "https://github.com/chronosphereio/calyptia-backend/releases/download/v3.0.2/calyptia-cli_3.0.2_darwin_all.tar.gz"
+    url "https://github.com/chronosphereio/calyptia-cli/releases/download/v3.0.2/calyptia-cli_3.0.2_darwin_all.tar.gz"
     sha256 "65ca7b01320d1cc323cbd865630759299161920203edcb357d1664d5abbbac4b"
 
     def install
@@ -20,7 +20,7 @@ class CalyptiaAT3 < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/chronosphereio/calyptia-backend/releases/download/v3.0.2/calyptia-cli_3.0.2_linux_amd64.tar.gz"
+        url "https://github.com/chronosphereio/calyptia-cli/releases/download/v3.0.2/calyptia-cli_3.0.2_linux_amd64.tar.gz"
         sha256 "d67f5a87eb23af9cb3c3ce361d768d6044621f96c9796eccf1ce4e7df37d87e9"
 
         def install
@@ -30,7 +30,7 @@ class CalyptiaAT3 < Formula
     end
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/chronosphereio/calyptia-backend/releases/download/v3.0.2/calyptia-cli_3.0.2_linux_arm64.tar.gz"
+        url "https://github.com/chronosphereio/calyptia-cli/releases/download/v3.0.2/calyptia-cli_3.0.2_linux_arm64.tar.gz"
         sha256 "654c471079bde60ab0106a912764e4be1e450624303e4d0929a3bd274fed061e"
 
         def install
