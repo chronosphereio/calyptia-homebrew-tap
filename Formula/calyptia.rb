8f942b0a1876a0ddc18f28a1979fdf3895d0eb87
Alec Holmes <alec@chronosphere.io>
2025-04-08 15:37:42 -0700

    Revert "Brew formula update for calyptia-cli version v3.38.0 (#43)"
    This reverts commit 66dced2541399ea55eec8842f11ee33c969008f3.

--- Formula/calyptia.rb
+++ Formula/calyptia.rb
@@ -5,12 +5,12 @@
 class Calyptia < Formula
   desc "Chronosphere Telemetry Pipelines CLI"
   homepage "https://docs.chronosphere.io/pipeline-cli"
-  version "3.38.0"
+  version "3.36.0"
   license "Apache-2.0"
 
   on_macos do
-    url "https://github.com/chronosphereio/calyptia-cli/releases/download/v3.38.0/calyptia-cli_3.38.0_darwin_all.tar.gz"
-    sha256 "f71d305ccb43bdfe60b0f4588a356b54d9e73c1391f8765a5d72906760d5ac7e"
+    url "https://github.com/chronosphereio/calyptia-cli/releases/download/v3.36.0/calyptia-cli_3.36.0_darwin_all.tar.gz"
+    sha256 "7c2f2d608cabef74b09958c45cae7806258101a2def3942e19bcf85268d58bc6"
 
     def install
       bin.install "calyptia"
@@ -20,8 +20,8 @@ class Calyptia < Formula
   on_linux do
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/chronosphereio/calyptia-cli/releases/download/v3.38.0/calyptia-cli_3.38.0_linux_amd64.tar.gz"
-        sha256 "2338a8c9db8e120b8d0dc1092a85c7614f287a2999747ef2612b377e82970704"
+        url "https://github.com/chronosphereio/calyptia-cli/releases/download/v3.36.0/calyptia-cli_3.36.0_linux_amd64.tar.gz"
+        sha256 "b04a947326530171180aa1d771f1438f1edbec580d7ad91b453b62648d5b9324"
 
         def install
           bin.install "calyptia"
@@ -30,8 +30,8 @@ class Calyptia < Formula
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/chronosphereio/calyptia-cli/releases/download/v3.38.0/calyptia-cli_3.38.0_linux_arm64.tar.gz"
-        sha256 "9279885ebd051ec72e2dec47a6f9c5c12416e980056e8beb11aacd1b9b97560f"
+        url "https://github.com/chronosphereio/calyptia-cli/releases/download/v3.36.0/calyptia-cli_3.36.0_linux_arm64.tar.gz"
+        sha256 "bb5fb08fa76b37ae8b6037baafffe5527440e22909c4a06538fa4f67d76e1227"
 
         def install
           bin.install "calyptia"
